fd2109f6ab86de83e600af576b492e79f0b28915
Eugene Safaris <monkeygenesafaris@gmail.com>
2026-07-31 21:30:41 +0100

    new page added

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
+  base: "/",
+
   plugins: [tailwindcss()],
+
+  build: {
+    outDir: "dist",
+
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        about: path.resolve(__dirname, "about/index.html"),
+        contact: path.resolve(__dirname, "contact/index.html"),
+        services: path.resolve(__dirname, "packages/index.html"),
+        portfolio: path.resolve(__dirname, "portfolio/index.html"),
+      },
+    },
+  },
 });
